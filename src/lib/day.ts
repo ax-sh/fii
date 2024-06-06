68896bb1d3f127e2cc884e74331c2d1c35f48d58
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-06 23:15:32 +0545

    feat: Add day.ts file with dayjs library and plugins

---
+++ src/lib/day.ts
@@ -0,0 +1,8 @@
+import * as dayjs from 'dayjs'
+import * as duration from 'dayjs/plugin/duration'
+import * as relativeTime from 'dayjs/plugin/relativeTime'
+dayjs.extend(relativeTime)
+dayjs.extend(duration)
+
+const day = dayjs
+export default day
