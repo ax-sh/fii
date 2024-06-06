ef27ec17903e9fe2e93a24ab562dc01277012c86
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-06 23:42:13 +0545

    feat: add db

--- src/commands/books/books.ts
+++ src/commands/books/books.ts
@@ -1,4 +1,5 @@
 import { GluegunCommand } from 'gluegun'
+import { execa } from "execa";
 
 // @see https://github.com/ciehanski/libgen-cli
 // @see https://github.com/freereadorg/awesome-libgen
@@ -6,15 +7,18 @@ const command: GluegunCommand = {
   name: 'books',
   alias: ['book'],
   run: async (toolbox) => {
+    // @ts-ignore
     const { print, system, parameters } = toolbox
     const name = parameters.string
     const cmd = `libgen-cli search ${name}`
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
     // const execa = require('execa')
-
-    const books = await system.run(cmd)
+    await execa(cmd, { shell: true, stdio: 'inherit' })
+    //
+    // const books = await system.run(cmd)
     // parameters.raw
     // print.info(name)
-    print.info(books)
+    // print.info(books)
   },
 }
 
