583c6246383a5a838b46a330e42ea457ffeb6238
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-06 23:28:55 +0545

    feat: Update book command to search for books based on provided name

--- src/commands/books/books.ts
+++ src/commands/books/books.ts
@@ -4,13 +4,13 @@ const command: GluegunCommand = {
   name: 'books',
   alias: ['book'],
   run: async (toolbox) => {
-    const { print, parameters } = toolbox
-    // const name = parameters.first
-    print.info(`parameters.raw, ${parameters.string}`)
-    // const books = await system.run('libgen-cli search')
+    const { print, system, parameters } = toolbox
+    const name = parameters.string
+
+    const books = await system.run(`libgen-cli search ${name}`)
     // parameters.raw
     // print.info(name)
-    // print.info(books)
+    print.info(books)
   },
 }
 
