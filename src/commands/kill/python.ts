17be626e8ce525cb1672a9c1cd573a451b40f3d2
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-07 01:17:40 +0545

    feat: add kill commands

---
+++ src/commands/kill/python.ts
@@ -0,0 +1,22 @@
+import type { GluegunCommand } from 'gluegun';
+
+import type { ExtendedToolbox } from '../../types';
+
+const command: GluegunCommand<ExtendedToolbox> = {
+  name: 'python',
+  alias: ['py'],
+  description: 'Kill all python processes',
+  run: async (toolbox) => {
+    const { print } = toolbox;
+    const spinner = print.spin('killing all python processes');
+
+    try {
+      await toolbox.killProcess('python.exe');
+    } catch (e) {
+      return spinner.warn('Killing failed');
+    }
+    spinner.succeed('Done');
+  }
+};
+
+module.exports = command;
