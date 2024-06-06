ea3e76f10fa6ea1b21866acc6ac5f7e0a890857a
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-06 23:03:35 +0545

    feat: Add query execution and logging in db module

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,12 @@
 import { drizzle } from 'drizzle-orm/bun-sqlite'
 import { Database } from 'bun:sqlite'
+import { sql } from 'drizzle-orm'
 
 const dbPath = 'sqlite.db'
 const sqlite = new Database(dbPath)
 export const db = drizzle(sqlite)
+
+const query = sql`select "hello world" as text`
+const result = db.get<{ text: string }[]>(query)
+console.log(result.length, result[0])
+// console.log(db);
