69b026194b18b905c705eca6741c94759ac3af6f
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-07 01:23:17 +0545

    feat: Add command to kill all chrome processes

---
+++ src/commands/kill/chrome.ts
@@ -0,0 +1,21 @@
+import type { GluegunCommand } from 'gluegun'
+
+import type { ExtendedToolbox } from '../../types'
+
+const command: GluegunCommand<ExtendedToolbox> = {
+  name: 'chrome',
+  description: 'Kill all chrome processes',
+  run: async (toolbox) => {
+    const { print } = toolbox
+    const spinner = print.spin('killing all chrome processes')
+
+    try {
+      await toolbox.killProcess('chrome.exe')
+    } catch (e) {
+      return spinner.warn('Killing failed')
+    }
+    spinner.succeed('Done')
+  },
+}
+
+module.exports = command
