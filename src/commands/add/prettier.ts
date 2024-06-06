7cdfbf679d340e53406ef97861e7b465f5f54a8a
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-07 00:26:00 +0545

    feat(add): add prettier command and templates for prettier config files

---
+++ src/commands/add/prettier.ts
@@ -0,0 +1,34 @@
+import { type GluegunCommand } from 'gluegun'
+
+import { type ExtendedToolbox } from '../../types'
+
+const command: GluegunCommand<ExtendedToolbox> = {
+  name: 'prettier',
+  alias: ['pretty'],
+  run: async (toolbox) => {
+    const { print, system, template, filesystem } = toolbox
+
+    const spinner = print.spin('Adding prettier deps')
+
+    await system.run(
+      'ni -D prettier pretty-quick @trivago/prettier-plugin-sort-imports',
+      { trim: true }
+    )
+    await toolbox.addScriptToPackageJson('pretty', 'pretty-quick')
+
+    spinner.info('Added prettier deps')
+
+    const fileNames = ['.prettierrc', '.prettierignore']
+    for (const fileName of fileNames) {
+      await template.generate({
+        // directory: "CONFIGS/tailwind",
+        template: `/CONFIGS/prettier/${fileName}.ejs`,
+        target: filesystem.path('.', fileName),
+      })
+    }
+    spinner.info('Added prettier configs')
+    spinner.stopAndPersist()
+  },
+}
+
+module.exports = command
