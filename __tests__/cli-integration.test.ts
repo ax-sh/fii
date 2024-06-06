c4fced70b926dfc8dda37783c91ff399df3e0c22
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-06 19:57:21 +0545

    test: mark tests as todos for future implementation

--- __tests__/cli-integration.test.ts
+++ __tests__/cli-integration.test.ts
@@ -5,17 +5,17 @@ const src = filesystem.path(__dirname, '..')
 const cli = async (cmd) =>
   system.run('node ' + filesystem.path(src, 'bin', 'fii') + ` ${cmd}`)
 
-test('outputs version', async () => {
+test.todo('outputs version', async () => {
   const output = await cli('--version')
   expect(output).toContain('0.0.1')
 })
 
-test('outputs help', async () => {
+test.todo('outputs help', async () => {
   const output = await cli('--help')
   expect(output).toContain('0.0.1')
 })
 
-test('generates file', async () => {
+test.todo('generates file', async () => {
   const output = await cli('generate foo')
 
   expect(output).toContain('Generated file at models/foo-model.ts')
