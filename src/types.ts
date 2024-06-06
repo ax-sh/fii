af6c26ce57b08de75957107b7a79003f24aa7bdd
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-06 23:54:55 +0545

    feat(types): add new classes and types for extended GluegunToolbox

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,22 @@
-// export types
+import { type GluegunToolbox, print } from 'gluegun'
+
+export class KnownError extends Error {
+  constructor(msg: string | string[]) {
+    let message: string
+    if (Array.isArray(msg)) {
+      message = msg.join('\n')
+    }
+    print.error(message)
+    super(message)
+  }
+}
+
+export class UsableBinaryNotFound extends Error {}
+
+type ProcessOptions = 'python.exe' | 'node.exe' | 'bun.exe' | string
+
+export type ExtendedToolbox = GluegunToolbox & {
+  addScriptToPackageJson(scriptName: string, cmd: string): Promise<void>
+  killProcess(killProcess: ProcessOptions): Promise<string>
+  mkCd(dirPath: ProcessOptions): Promise<void>
+}
