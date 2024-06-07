030fdeb4f83215a83cf9392870e0ded5bc885def
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-08 03:41:20 +0545

    chore: format code with Prettier

--- .xo-config.js
+++ .xo-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   space: true,
-  semicolon:false,
+  semicolon: false,
   // prettier:true,
-  "rules": {
-    "import/extensions": "off",
+  rules: {
+    'import/extensions': 'off',
     // TODO research what unicorn/prefer-module does
-    "unicorn/prefer-module": "off",
-    "unicorn/prevent-abbreviations": "off",
+    'unicorn/prefer-module': 'off',
+    'unicorn/prevent-abbreviations': 'off',
 
-    "@typescript-eslint/object-curly-spacing": "off",
-    "@typescript-eslint/prefer-nullish-coalescing":"warn"
-  }
-};
+    '@typescript-eslint/object-curly-spacing': 'off',
+    '@typescript-eslint/prefer-nullish-coalescing': 'warn',
+  },
+}
