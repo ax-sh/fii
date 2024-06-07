bf630bd3bc6d066f82511fd1ac02d5ac206bfb03
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-08 03:21:50 +0545

    feat: Add XO configuration file with ESLint rules

---
+++ .xo-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  space: true,
+  "rules": {
+    "import/extensions": "off",
+    "@typescript-eslint/semi": "off",
+    "@typescript-eslint/prefer-nullish-coalescing":"warn"
+  }
+};
