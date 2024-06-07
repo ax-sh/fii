6ac6c746db41727682da0893f3b976e0db990485
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-08 03:31:26 +0545

    feat: ignore rule in xo

--- .xo-config.js
+++ .xo-config.js
@@ -4,6 +4,7 @@ module.exports = {
   // prettier:true,
   "rules": {
     "import/extensions": "off",
+    "unicorn/prevent-abbreviations": "off",
 
     "@typescript-eslint/object-curly-spacing": "off",
     "@typescript-eslint/prefer-nullish-coalescing":"warn"
