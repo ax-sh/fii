28506ff9242fb88abbddc477c6248e29d1c7f888
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-08 03:06:28 +0545

    chore: format code with Prettier

--- src/lib/add-vitest-types-to-tsconfig.ts
+++ src/lib/add-vitest-types-to-tsconfig.ts
@@ -1,12 +1,12 @@
-import { filesystem } from 'gluegun';
-import { applyEdits, modify, parse } from 'jsonc-parser';
+import { filesystem } from 'gluegun'
+import { applyEdits, modify, parse } from 'jsonc-parser'
 
 export const sanityTest = `
       function sum(a: number, b: number) {return a + b}
       
       test('adds 1 + 2 to equal 3', async () => {
         expect(sum(1, 2)).toBe(3)
-      })`;
+      })`
 
 export const vitestConfigContent = `
     import { configDefaults, defineConfig } from 'vitest/config';
@@ -19,21 +19,21 @@ export const vitestConfigContent = `
         globals: true,
       }
     });
-`;
+`
 
 export async function addVitestTypesToTsconfig(tsconfigPath: string) {
-  const data = filesystem.read(tsconfigPath);
-  const tsconfig = parse(data);
-  const types = tsconfig.compilerOptions.types;
+  const data = filesystem.read(tsconfigPath)
+  const tsconfig = parse(data)
+  const types = tsconfig.compilerOptions.types
   if (!types) {
-    const vitestTypes = ['vitest/globals'];
+    const vitestTypes = ['vitest/globals']
 
     // Prepare the edits to add the new property
     const edits = modify(data, ['compilerOptions', 'types'], vitestTypes, {
-      formattingOptions: { insertSpaces: true, tabSize: 2 }
-    });
+      formattingOptions: { insertSpaces: true, tabSize: 2 },
+    })
     // Apply the edits to the original JSONC data
-    const updatedData = applyEdits(data, edits);
-    filesystem.write(tsconfigPath, updatedData);
+    const updatedData = applyEdits(data, edits)
+    filesystem.write(tsconfigPath, updatedData)
   }
 }
