c48a02c23a63d9bcdd74d38a2bd627c03a864e1c
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-08 03:22:12 +0545

    style: remove unnecessary comment and fix typo in semicolon option

--- .xo-config.js
+++ .xo-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   space: true,
+  semicolon:false,
   "rules": {
     "import/extensions": "off",
-    "@typescript-eslint/semi": "off",
+    // "@typescript-eslint/semi": "off",
     "@typescript-eslint/prefer-nullish-coalescing":"warn"
   }
 };
