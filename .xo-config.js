fbbb9f326a033b4962240be087c5662ecef6344b
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-08 03:23:22 +0545

    style: Add prettier option and remove unnecessary comment in .xo-config.js

--- .xo-config.js
+++ .xo-config.js
@@ -1,8 +1,10 @@
 module.exports = {
   space: true,
   semicolon:false,
+  "prettier":true,
   "rules": {
     "import/extensions": "off",
+
     // "@typescript-eslint/semi": "off",
     "@typescript-eslint/prefer-nullish-coalescing":"warn"
   }
