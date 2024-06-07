aa761425862cee7f3b8746d67af632a9c805e332
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-08 03:35:52 +0545

    docs: Add comment for disabling unicorn/prefer-module rule

--- .xo-config.js
+++ .xo-config.js
@@ -4,6 +4,8 @@ module.exports = {
   // prettier:true,
   "rules": {
     "import/extensions": "off",
+    // TODO research what unicorn/prefer-module does
+    "unicorn/prefer-module": "off",
     "unicorn/prevent-abbreviations": "off",
 
     "@typescript-eslint/object-curly-spacing": "off",
