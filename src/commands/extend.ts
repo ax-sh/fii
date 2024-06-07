587c1176b11f4ee8e2ad389cc0a58e9fd93b6b28
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-08 03:57:41 +0545

    docs: Add description for the 'extend' command

--- src/commands/extend.ts
+++ src/commands/extend.ts
@@ -3,6 +3,7 @@ import { ExtendedToolbox } from '../types'
 
 const command: GluegunCommand<ExtendedToolbox> = {
   name: 'extend',
+  description: 'Open Fii project on a webstorm editor for further extension',
   run: async (toolbox) => {
     const { print, system, filesystem } = toolbox
     print.info('Tools for development')
