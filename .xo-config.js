279c6bb7be298640d393ba6b965ba1d4cae300e6
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-08 03:46:31 +0545

    feat: Add overrides for '@typescript-eslint/no-unsafe-argument' in 'src/cli.ts'

--- .xo-config.js
+++ .xo-config.js
@@ -7,9 +7,19 @@ module.exports = {
     // TODO research what unicorn/prefer-module does
     'unicorn/prefer-module': 'off',
     'unicorn/prevent-abbreviations': 'off',
+    '@typescript-eslint/array-type': 'off',
+    'capitalized-comments': 'off',
 
     '@typescript-eslint/object-curly-spacing': 'off',
     // TODO research what @typescript-eslint/prefer-nullish-coalescing does
     '@typescript-eslint/prefer-nullish-coalescing': 'off',
   },
+  overrides: [
+    {
+      files: 'src/cli.ts',
+      rules: {
+        '@typescript-eslint/no-unsafe-argument': 'off',
+      },
+    },
+  ],
 }
