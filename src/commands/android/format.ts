0c19216d280c982cb903c1027772f231811529c3
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-09 14:10:43 +0545

    feat: Add android status command to format project status

---
+++ src/commands/android/format.ts
@@ -0,0 +1,16 @@
+
+
+import { type GluegunCommand } from 'gluegun'
+// https://chatgpt.com/c/50e14c29-fe89-4825-9dc1-da19a1b90d10
+const command: GluegunCommand = {
+  name: 'status',
+  run: async (toolbox) => {
+    const { print, system } = toolbox
+    const spinner = print.spin('Formatting project status of servers')
+
+    const status = await system.run('ktlint -F "**/*.kt"')
+    spinner.succeed(status)
+  },
+}
+
+module.exports = command
