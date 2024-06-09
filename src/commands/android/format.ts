53319c5f8d5a216e2066e11f94df0fd4ab607732
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-09 17:06:44 +0545

    chore: format code with Prettier

--- src/commands/android/format.ts
+++ src/commands/android/format.ts
@@ -2,7 +2,7 @@ import { type GluegunCommand } from 'gluegun'
 // https://chatgpt.com/c/50e14c29-fe89-4825-9dc1-da19a1b90d10
 const command: GluegunCommand = {
   name: 'format',
-  alias:['fmt', 'f'],
+  alias: ['fmt', 'f'],
   run: async (toolbox) => {
     const { print, system } = toolbox
     const spinner = print.spin('Formatting project status of servers')
