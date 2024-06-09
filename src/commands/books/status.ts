b3794b0ce8469c86b57fec3bb06e7cfa50a17520
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-09 14:53:36 +0545

    chore: remove unused cmd

--- src/commands/books/status.ts
+++ src/commands/books/status.ts
@@ -1,11 +1,17 @@
 import { type GluegunCommand } from 'gluegun'
 
+// @see https://github.com/ciehanski/libgen-cli
+// @see https://github.com/freereadorg/awesome-libgen
 const command: GluegunCommand = {
   name: 'status',
   run: async (toolbox) => {
     const { print, system } = toolbox
     const spinner = print.spin('Getting status of servers')
-
+    // `libgen-cli search -e pdf ${name}`
+    // const execa = require('execa')
+    // await execa(cmd, { shell: true, stdio: 'inherit' })
+    //
+    // const books = await system.run(cmd)
     const status = await system.run('libgen-cli status')
     spinner.succeed(status)
   },
