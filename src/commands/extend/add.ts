e3d8b000a23d08a233a5bd6c933165d554840727
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-13 14:53:39 +0545

    feat: update command generation logic

--- src/commands/extend/add.ts
+++ src/commands/extend/add.ts
@@ -1,24 +1,26 @@
 import { type GluegunCommand } from 'gluegun'
-import { type ExtendedToolbox, KnownError } from "../../types";
+import { type ExtendedToolbox, KnownError } from '../../types'
 
 const command: GluegunCommand<ExtendedToolbox> = {
   name: 'add',
   description: 'generate and add new script for fii add subcommand',
   run: async (toolbox) => {
-    const { print, template, parameters } = toolbox
+    const { print, template, parameters, filesystem } = toolbox
     const name = parameters.first.trim()
-    if(!name)throw new KnownError("No command name found.")
-    const path = `src/commands/add/${name}.ts`
+    if (!name) throw new KnownError('No command name found.')
 
-    // const target = await toolbox.cliAppDir(path)
+    const cliProjectPath = filesystem.path(__dirname, '..', '..', '..')
+    process.chdir(cliProjectPath)
+    const target = `src/commands/add/${name}.ts`
+    print.info(`Generating subcommand ${name} on ${cliProjectPath} ${target}`)
 
     await template.generate({
       template: 'command.ts.ejs',
-      target: path,
+      target,
       props: { name },
     })
     print.info(
-      `Tools generate and add new script for fii add subcommand ${name}`,
+      `Generated subcommand fii extend add ${name}`,
     )
   },
 }
