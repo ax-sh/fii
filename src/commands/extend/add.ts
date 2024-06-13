97525bb03c9baf12dac5ec52c98836c5b8a53523
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-13 14:46:19 +0545

    fix: Fix trimming of command name and add validation for empty name

--- src/commands/extend/add.ts
+++ src/commands/extend/add.ts
@@ -1,17 +1,20 @@
 import { type GluegunCommand } from 'gluegun'
-import { type ExtendedToolbox } from '../../types'
+import { type ExtendedToolbox, KnownError } from "../../types";
 
 const command: GluegunCommand<ExtendedToolbox> = {
   name: 'add',
   description: 'generate and add new script for fii add subcommand',
   run: async (toolbox) => {
     const { print, template, parameters } = toolbox
-    const name = parameters.first
-    // todo check if in right path
+    const name = parameters.first.trim()
+    if(!name)throw new KnownError("No command name found.")
+    const path = `src/commands/add/${name}.ts`
+
+    // const target = await toolbox.cliAppDir(path)
 
     await template.generate({
       template: 'command.ts.ejs',
-      target: `src/commands/add/${name}.ts`,
+      target: path,
       props: { name },
     })
     print.info(
