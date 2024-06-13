188261da6fdc5fda7e4a4d4702fbb535e763e1bf
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-13 15:52:15 +0545

    feat: Improve vite project setup by installing additional dependencies and changing directory

--- src/commands/add/vite/m.ts
+++ src/commands/add/vite/m.ts
@@ -4,18 +4,24 @@ const command: GluegunCommand = {
   name: 'm',
   description: 'Create a bare minimum vite react ts project',
   run: async (toolbox) => {
-    const { print, parameters, system,filesystem } = toolbox
-    const appName = parameters.first;
+    const { print, parameters, system, filesystem } = toolbox
+    const appName = parameters.first
 
-    const installedPath = filesystem.path(appName);
+    const installedPath = filesystem.path(appName)
     if (filesystem.isDirectory(installedPath)) {
-      return print.error(`Folder with the name [${appName}] already exists`);
+      return print.error(`Folder with the name [${appName}] already exists`)
     }
-    const spinner = print.spin(`Creating vite app with react swc and tailwind [${appName}]`);
-    await system.run(`bun create vite ${appName} --template react-swc-ts`);
-    spinner.info(`Created vite app [${appName}]!`);
-    spinner.info('Adding prettier and tailwind');
-    spinner.succeed(`Created App ${appName}`);
+    const spinner = print.spin(
+      `Creating vite app with react swc and tailwind [${appName}]`,
+    )
+    await system.run(`bun create vite ${appName} --template react-swc-ts`)
+    spinner.info(`Created vite app [${appName}]!`)
+    process.chdir(installedPath)
+    spinner.info(`Installing clsx and prettier!`)
+    await system.run('bun i && ni clsx && fii add prettier')
+
+    // spinner.info('Adding prettier and tailwind');
+    spinner.succeed(`Created App ${appName}`)
   },
 }
 
