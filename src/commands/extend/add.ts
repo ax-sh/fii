50e1c4ca2546e0ce4417a5ef670164223d6f55d4
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-13 14:35:31 +0545

    feat: add command.ts.ejs

--- src/commands/extend/add.ts
+++ src/commands/extend/add.ts
@@ -5,8 +5,18 @@ const command: GluegunCommand<ExtendedToolbox> = {
   name: 'add',
   description: 'generate and add new script for fii add subcommand',
   run: async (toolbox) => {
-    const { print } = toolbox
-    print.info('Tools generate and add new script for fii add subcommand')
+    const { print, template, parameters } = toolbox
+    const name = parameters.first
+    // todo check if in right path
+
+    await template.generate({
+      template: 'command.ts.ejs',
+      target: `src/commands/add/${name}.ts`,
+      props: { name },
+    })
+    print.info(
+      `Tools generate and add new script for fii add subcommand ${name}`,
+    )
   },
 }
 
