91b9ad6a00ff2cfed59661bcf3a26323eb8686de
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-14 23:53:55 +0545

    feat: Add global node configuration in eslint config

--- src/templates/CONFIGS/eslint/eslint.config.mjs
+++ src/templates/CONFIGS/eslint/eslint.config.mjs
@@ -1,11 +1,19 @@
 import pluginJs from '@eslint/js'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import eslintConfigPrettier from 'eslint-config-prettier'
+import globals from 'globals';
 
 const ignores = {
   ignores: ['.config/*', 'build/', '.xo-config.js', '.*.js'],
 }
 const eslintConfigs = [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
   pluginJs.configs.recommended,
   eslintPluginPrettierRecommended,
   eslintConfigPrettier,
