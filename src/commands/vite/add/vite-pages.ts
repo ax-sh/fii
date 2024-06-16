c8c3137f8ed944878bb44681a833d60e7cdcaf21
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-16 11:12:20 +0545

    feat: add vite plugin vite-pages

---
+++ src/commands/vite/add/vite-pages.ts
@@ -0,0 +1,17 @@
+import type { GluegunCommand } from 'gluegun'
+import { type ExtendedToolbox } from '../../../types'
+
+const command: GluegunCommand<ExtendedToolbox> = {
+  name: 'vite-pages',
+  run: async (toolbox) => {
+    // @ts-expect-error
+    const { print, system } = toolbox
+    const spinner = print.spin("Adding vite-pages")
+
+    spinner.succeed(`Added vite-pages`)
+    // await system.run('ni -D vite-plugin-pages')
+    // await system.run('ni react-router react-router-dom')
+  },
+}
+
+module.exports = command
