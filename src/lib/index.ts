cf1a554fae99671fe990798c00574159512a9a4a
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-09-19 20:50:19 +0545

    refactor(index.ts): streamline import and function syntax

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,10 +1,10 @@
 import { filesystem } from 'gluegun'
 
-// import * as day from './day'
-//
-// export function elapsedDuration(date: Date) {
-//   return day.duration(day(date).diff())
-// }
+import day from './day'
+
+export function elapsedDuration(date: Date) {
+  return day.duration(day(date).diff())
+}
 
 const fiiUserDirJoin = async (...paths: string[]) =>
   filesystem.dirAsync(filesystem.path(filesystem.homedir(), '.ax-sh/.fii', ...paths))
