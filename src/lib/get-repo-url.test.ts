0b152047052eee33a7841b8b57883ea4cefb9ca0
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-09-19 16:44:24 +0545

    test(get-repo-url): update tests for repo URL functionality

--- src/lib/get-repo-url.test.ts
+++ src/lib/get-repo-url.test.ts
@@ -2,7 +2,13 @@ import * as gh from './get-repo-url'
 
 vi.mock('./get-repo-url', { spy: true })
 describe('getRepoBaseName', () => {
-  it('should correctly parse', async () => {
+  it('should correctly get repo url', async () => {
+    const baseName = await gh.getRepoBaseName()
+    console.log(baseName)
+    expect(gh.getRepoUrl()).resolves.toBe('https://github.com/ax-sh/fii')
+  })
+
+  it('should correctly get mocked repo url', async () => {
     const baseName = await gh.getRepoBaseName()
     console.log(baseName)
     expect(gh.getRepoUrl()).resolves.toBe('https://github.com/ax-sh/fii')
