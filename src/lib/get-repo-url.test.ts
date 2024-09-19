72c5238dccb655e4263f38dfa156c1aba9923162
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-09-19 19:44:27 +0545

    test(get-repo-url): add tests for getRepoUrl function

--- src/lib/get-repo-url.test.ts
+++ src/lib/get-repo-url.test.ts
@@ -9,8 +9,40 @@ describe('getRepoBaseName', () => {
   })
 
   it('should correctly get mocked repo url', async () => {
+    const mockVal = 'https://github.com/user/repok.git'
+
+    // gh.getRepoUrl.mockResolvedValue(mockVal)
+    // const baseName = await gh.getRepoBaseName()
+    const w = gh.getRepoUrl()
+
+    await expect(w).resolves.toHaveBeenCalled()
+    await expect(w).resolves.toBe('https://github.com/ax-sh/repo')
+  })
+
+  it('should correctly get mocked repo url v2', async () => {
+    vi.mocked(gh.getRepoUrl).mockResolvedValue('https://github.com/user/repo.git')
+
     const baseName = await gh.getRepoBaseName()
+    // Assert
+    // expect(gh.getRepoUrl).toHaveBeenCalled()
     console.log(baseName)
-    expect(gh.getRepoUrl()).resolves.toBe('https://github.com/ax-sh/fii')
+    expect(gh.getRepoUrl()).resolves.toBe('https://github.com/ax-sh/repo')
+  })
+
+  it('should return the base name of the repo from the URL', async () => {
+    // Mock the getRepoUrl function
+    const getRepoUrlMock = vi
+      .spyOn(gh, 'getRepoUrl')
+      .mockResolvedValue('https://github.com/user/repo.git')
+
+    // Act
+    const baseName = await gh.getRepoBaseName()
+
+    // Assert
+    expect(gh.getRepoUrl).toHaveBeenCalled()
+    // expect(baseName).toBe('repo.git') // path.parse(url).base will be 'repo.git'
+    //
+    // // Clean up
+    // getRepoUrlMock.mockRestore()
   })
 })
