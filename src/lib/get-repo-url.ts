69c5f9575b73fa1208fd2c8998405b2c934c4e73
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-09-19 20:16:34 +0545

    refactor: move git flow init to helper function

--- src/lib/get-repo-url.ts
+++ src/lib/get-repo-url.ts
@@ -5,13 +5,15 @@ import { getRepoUrl } from './helpers/git-utils'
 export async function getRepoBaseName() {
   const url = await getRepoUrl()
   const parsed = path.parse(url)
-  // {
-  //   root: '',
-  //   dir: 'https://github.com/ax-sh',
-  //   base: 'fii',
-  //   ext: '',
-  //   name: 'fii',
-  // }
+  /**
+   * {
+   *   root: '',
+   *   dir: 'https://github.com/ax-sh',
+   *   base: 'fii',
+   *   ext: '',
+   *   name: 'fii',
+   * }
+   */
   const repoName = parsed.name.trim()
 
   return repoName
--- src/lib/helpers/git-utils.ts
+++ src/lib/helpers/git-utils.ts
@@ -3,3 +3,7 @@ import { system } from 'gluegun'
 export async function getRepoUrl() {
   return system.run('git remote get-url origin', { trim: true })
 }
+
+export async function gitFlowInit() {
+  return system.run(`git flow init -d && git add . && git commit -m init`)
+}
