83ddf0d5d70ee125f5bce6907ed962763b75098f
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-09-21 16:50:56 +0545

    feat: add python outdated command

---
+++ src/commands/py/outdated.ts
@@ -0,0 +1,23 @@
+import type { GluegunCommand } from 'gluegun'
+import { type ExtendedToolbox } from '../../types'
+
+const command: GluegunCommand<ExtendedToolbox> = {
+  name: 'outdated',
+  run: async (toolbox) => {
+    const { system } = toolbox
+
+    // const spinner = print.spin(`Adding py ${name}`)
+    const hasPipReview = system.which('pip-review')
+    if(hasPipReview) {
+      await system.run('pip install pip-review')
+    }
+    console.log(hasPipReview)
+
+
+    await system.run('pip list --o')
+
+    // spinner.succeed(`Added py ${name}`)
+  },
+}
+
+module.exports = command
---
+++ src/commands/py/py.ts
@@ -0,0 +1,18 @@
+import type { GluegunCommand } from 'gluegun'
+import { type ExtendedToolbox } from '../../types'
+
+const command: GluegunCommand<ExtendedToolbox> = {
+  name: 'py',
+  run: async (toolbox) => {
+    const { print, parameters, system } = toolbox
+    const name = parameters.first
+    const spinner = print.spin(`Adding py ${name}`)
+
+    print.info(`Todo py ${name}`)
+    await system.run('echo ni -D husky')
+
+    spinner.succeed(`Added py ${name}`)
+  },
+}
+
+module.exports = command
