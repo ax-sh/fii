f2ca5c11b52a891adf52c2bb6b97b6595470470a
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-09-22 23:44:31 +0545

    feat: add kolorist package for better error handling

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,9 @@
 import type { BrowserCookiesSingleton } from '@ax-sh/browser-cookies'
 import type { FSJetpack } from 'fs-jetpack/types'
 import { type GluegunToolbox, print } from 'gluegun'
+import { dim } from 'kolorist'
 
+// import { version } from '../pa'
 import type { addScriptToPackageJson } from './lib/cli'
 
 export class KnownError extends Error {
@@ -13,6 +15,17 @@ export class KnownError extends Error {
 
     print.error(message)
     super(message)
+  }
+}
+// const indent = '    '
+export const handleCliError = (error: unknown) => {
+  if (error instanceof Error && !(error instanceof KnownError)) {
+    if (error.stack) {
+      console.error(dim(error.stack.split('\n').slice(1).join('\n')))
+    }
+    // console.error(`\n${indent}${dim(`  v${version}`)}`)
+    // console.error(`\n${indent}Please open a Bug report with the information above:`)
+    // console.error(`${indent}https://github.com/Nutlope/aicommits/issues/new/choose`)
   }
 }
 
