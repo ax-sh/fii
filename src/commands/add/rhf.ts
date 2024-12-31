f45da80537e1a29512406cbf9371bdd6fb6125bb
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-12-31 18:44:35 +0545

    chore: update package versions and add command description

--- src/commands/add/rhf.ts
+++ src/commands/add/rhf.ts
@@ -4,12 +4,14 @@ import type { ExtendedToolbox } from '../../types'
 
 const command: GluegunCommand<ExtendedToolbox> = {
   name: 'rhf',
+  description: 'adds react hook form on a node/bun repo',
   run: async (toolbox) => {
     const { print, system } = toolbox
 
     const spinner = print.spin('Adding React hook forms')
 
     await system.run('ni react-hook-form @hookform/error-message @hookform/resolvers zod')
+    // todo add basic test using react testing lib
 
     spinner.succeed('Added React hook forms')
   },
