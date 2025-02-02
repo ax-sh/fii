1c1ec3665fb1e54a38ae5eb635c3f822724f5351
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-02-03 04:16:45 +0545

    feat: add service generation to extend add command

--- src/commands/extend/add.ts
+++ src/commands/extend/add.ts
@@ -6,22 +6,31 @@ const command: GluegunCommand<ExtendedToolbox> = {
   name: 'add',
   description: 'generate and add new script for fii add subcommand',
   run: async (toolbox) => {
-    const { print, template, parameters, filesystem } = toolbox
+    const { parameters, template, print, system } = toolbox
     const name = parameters.first.trim()
     if (!name) {
       throw new KnownError('No command name found.')
     }
-
-    const cliProjectPath = filesystem.path(__dirname, '..', '..', '..')
+    const { appRootPathJoin, cliProjectPath } = await import('../../lib')
     process.chdir(cliProjectPath)
-    const target = `src/commands/add/${name}.ts`
-    print.info(`Generating subcommand ${name} on ${cliProjectPath} ${target}`)
+    const commandPath = appRootPathJoin(`src/commands/${name}.ts`)
+    const commandServicePath = appRootPathJoin(`src/services/${name}.ts`)
+    print.info(`Generating subcommand ${name} on ${cliProjectPath} ${commandPath}`)
 
     await template.generate({
       template: 'command.ts.ejs',
-      target,
+      target: commandPath,
       props: { name },
     })
+    await template.generate({
+      template: 'service.ts.ejs',
+      target: commandServicePath,
+      props: { name },
+    })
+
+    print.info(`Generated file at models/${name}-model.ts`)
+    await system.run('nr pretty')
+    print.printCommands(toolbox)
     print.success(`Generated subcommand fii extend add ${name}`)
   },
 }
