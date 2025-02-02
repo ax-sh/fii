5c842aefde984542a27aa0efb99e471c9764931c
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-02-03 04:13:18 +0545

    feat: wip

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
-import * as os from 'node:os'
 import * as jetpack from 'fs-jetpack'
+import * as os from 'node:os'
 
 // dayjs import caused error fix later
 // import { type FSJetpack } from 'fs-jetpack/types'
@@ -13,3 +13,7 @@ export const fiiUserDirJoin = async (...paths: string[]) =>
   jetpack.dirAsync(jetpack.path(os.homedir(), '.ax-sh/.fii', ...paths))
 
 export const cliProjectPath = jetpack.path(__dirname, '..', '..')
+
+export function appRootPathJoin(...path: string[]): string {
+  return jetpack.path(cliProjectPath, ...path)
+}
